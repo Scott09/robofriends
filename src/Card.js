a332777e1b008969edc1bc4aba7ed3a66a9f3e57
Scott Appleton <scottappleton09@gmail.com>
2019-10-07 23:18:43 -0700

    created card component

---
+++ src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Card = (props) => {
+  return (
+    <div>
+      <img src="https://robohash.org/test?200x200" alt="robot"/>
+      <div>
+        <h2>Scott</h2>
+        <p>scott@scott.com</p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
