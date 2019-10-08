f12d489b00996fb6d140f2b8196041ff104c52ca
Scott Appleton <scottappleton09@gmail.com>
2019-10-07 21:37:33 -0700

    add tachyons dependency.

--- src/Hello.js
+++ src/Hello.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 
-export default class Hello extends Component {
+class Hello extends Component {
   render() {
     return(
-      <div className="container">
+      <div className="f1 tc">
         <h1>Hello React World!</h1>
+        <h2>Welcome to React</h2>
       </div>
     )
   }
 }
+
+export default Hello;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Hello from './Hello.js';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
 
 ReactDOM.render(<Hello />, document.getElementById('root'));
 
