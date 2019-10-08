4fa94eb3cc0188ae4c1d96ebb190e87e6d03fc67
Scott Appleton <scottappleton09@gmail.com>
2019-10-07 19:14:43 -0700

    created new react app. changed functional component to class based component

---
+++ src/Hello.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Hello extends Component {
+  render() {
+    return(
+      <div className="container">
+        <h1>Hello React World!</h1>
+      </div>
+    )
+  }
+}
