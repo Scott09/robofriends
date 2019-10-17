ab4820e283c52d15c2f84601e24b7534c5bc13b8
Scott Appleton <scottappleton09@gmail.com>
2019-10-17 01:51:05 -0700

    Updates

---
+++ precache-manifest.24eecda271ab23b79a26e8e82440a506.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8950d4bb4560e10561424187b2f5b8c",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "ba806259a0963236db79",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7ec743238ac0e2f3b5c4",
+    "url": "/robofriends/static/css/main.25977b6d.chunk.css"
+  },
+  {
+    "revision": "ba806259a0963236db79",
+    "url": "/robofriends/static/js/2.a7b5e542.chunk.js"
+  },
+  {
+    "revision": "7ec743238ac0e2f3b5c4",
+    "url": "/robofriends/static/js/main.d6c4085c.chunk.js"
+  },
+  {
+    "revision": "15646e7ae3f7e7d4e926",
+    "url": "/robofriends/static/js/runtime-main.59eca5ac.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
